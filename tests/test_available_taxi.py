245e39b9d098b95095f8f3d75ac9ee33673d6eca
Alejandro Seif <seifalejandro@gmail.com>
2021-11-27 13:53:41 +0800

    upgrated unit test on taxis

--- tests/test_available_taxi.py
+++ tests/test_available_taxi.py
@@ -8,3 +8,9 @@ def test_url():
 def test_response():
     taxi.get_response()
     assert len(taxi.response) > 0
+
+def test_amount_taxis():
+    taxi.get_response()
+    taxi.taxi_analytics()
+    assert taxi.available_taxi_count > 0
+    assert taxi.available_taxi_count < 100000
